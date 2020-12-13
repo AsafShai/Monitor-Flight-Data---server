62d6ab1f7512e3dd25ebf6d59716698bc3bf9e3d
Asaf Shai <asafshai211@gmail.com>
2020-12-13 14:25:34 +0200

    get inputs of the data and sends to the connected client

---
+++ FlightData.js
@@ -0,0 +1,10 @@
+class FlightData {
+    
+    constructor(altitude, his, adi) {
+        this.altitude = altitude;
+        this.his = his;
+        this.adi = adi;
+    }
+}
+
+module.exports = FlightData;
--- server.js
+++ server.js
@@ -1,5 +1,10 @@
+const FlightData = require('./FlightData');
+
+const prompt = require('prompt');
+
 const express = require('express');
 const httpServer = require('http');
+const { resolve } = require('path');
 app = express();
 server = httpServer.createServer(app);
 const io = require('socket.io')(server, {
@@ -8,29 +13,76 @@ const io = require('socket.io')(server, {
     }
 });
 
-
-
 const PORT = 4000;
-//var io = socketIo(server);
-// var io = socketIo(PORT);
-//io.listen(PORT);
+
+var schema = {
+    properties: {
+        Altitude: {
+            conform: function(input) {
+                return input >= 0 && input <= 3000;
+            },
+            message: "Altitude must be in range of 0 to 3000",
+            required: true
+        },
+        HIS: {
+            conform: function(input) {
+                return input >= 0 && input <= 360;
+            },
+            message: "HIS must be in range of 0 to 360",
+            required: true
+        },
+        ADI: {
+            conform: function(input) {
+                return input >= -100 && input <= 100;
+            }, 
+            message: "ADI must be in range of -100 to 100",
+            required: true
+        }
+    }
+}; 
+
+prompt.start();
+
+const InputData = (socket) => {
+    return new Promise((resolve, reject) => {
+        prompt.get(schema, function (err, result) {
+            if (err) {
+                console.log(err);
+                reject();
+            }
+            else {
+                resolve(new FlightData(result.Altitude, result.HIS, result.ADI));
+            }
+        });
+    })
+}
 
 
-
-
-// io.listen(PORT, () =>{ 
-//     console.log("listening on port " + PORT)
-// });
-
+const keyPress = async () => {
+    process.stdin.setRawMode(true);
+    return new Promise(resolve = process.stdin.once('data', () => {
+        process.stdin.setRawMode(false);
+        resolve();
+    })).catch(err => console.log(err)) 
+    
+}
 
 server.listen(PORT, () =>{ 
     console.log("listening on port " + PORT )
 });
 
-io.on('connection', socket =>{
+io.on('connection', async (socket) =>{
     console.log("client connected");
-    socket.emit("sendingData", "message");
+
     socket.on("disconnect", () => {
         console.log("client disconnected");
     })
+
+    while (true) {
+        const flightData = await InputData(socket);
+        //await keyPress();
+        console.log("finished");
+        socket.emit("sendingData", flightData);
+    }
+
 });
